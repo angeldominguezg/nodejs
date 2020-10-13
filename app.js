b50796dd2be15b1a114814b1bd7d692515d51ab3
Angel Dominguez <angeldominguezg@gmail.com>
2020-10-13 10:56:30 -0400

    Initals routes and middleware

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
-
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log("In the second middleware");
-    res.send('<h1>Hello from experss!!!</h1>');
-    // next();
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
