66b92b062149a16915dadeb4a380b9537faeb6ad
Angel Dominguez <angeldominguezg@gmail.com>
2020-11-10 08:31:27 -0400

    Clean pug and handlebars templates

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,7 +18,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', { pageTitle: 'Page not found!' });
 });
 
