330d3639a8edd9dabc309aa4749e93e096117a1f
Angel Dominguez <angeldominguezg@gmail.com>
2020-11-24 07:13:39 -0400

    Added Add to cart post request

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,6 +53,12 @@ exports.getCart = (req, res, next) => {
     });
 };
 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+}
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
--- routes/shop.js
+++ routes/shop.js
@@ -4,12 +4,13 @@ const shopController = require('../controllers/shop')
 
 const router = express.Router();
 
-
 router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
 
 router.get('/chekout', shopController.getCheckout);
 
