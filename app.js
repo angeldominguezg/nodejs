7ab5845a20c21326bbe49915ed4c328c4c185c0a
Angel Dominguez <angeldominguezg@gmail.com>
2020-12-28 18:46:53 -0400

    Added user and product database relationship

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error')
 
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const bodyParser = require('body-parser');
 
@@ -17,14 +19,39 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then( user=> {
+            req.user = user;
+            next();
+        })
+        .catch( err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize.sync()
     .then( result  => {
+        return User.findByPk(1);
         // console.log('[sequelize]', result)
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({
+                name: 'Angel',
+                email: 'testing@mail.com'
+            });
+        }
+        return user
+    })
+    .then( user => {
+        console.log(user);
         app.listen(3000);
     })
     .catch( err => {
