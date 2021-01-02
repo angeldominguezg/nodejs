2bbf24e8d7e01b0ce5bde87a5dcc4cef603d8827
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-02 14:05:07 -0400

    Added mongodb connection node package and configuration

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://adominguez:4MJeRR7pxoN10hnT@cluster0.xeicl.mongodb.net/<dbname>?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('connected!');
+            callback(client);
+        })
+        .catch( err => console.log(err));
+}
+
+module.exports = mongoConnect;
