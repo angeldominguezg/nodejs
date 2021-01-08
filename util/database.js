d3391cc24d9caa7e5d268df0b067ee91476b27a1
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-08 19:51:38 -0400

    Fix dotenv request variables

--- util/database.js
+++ util/database.js
@@ -1,13 +1,12 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const dotenv = require('dotenv');
-const env = dotenv.config();
+require('dotenv').config();
 
 let _db;
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://'+env.parsed.DB_USER+':'+env.parsed.DB_PASSWORD+'@cluster0.xeicl.mongodb.net/shop?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@cluster0.xeicl.mongodb.net/shop?retryWrites=true&w=majority')
         .then(client => {
             console.log('connected!');
             _db = client.db();
