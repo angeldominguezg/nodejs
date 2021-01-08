e8b18a218e6b31ba7aca33434dbe4deaf5ca2c46
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-08 13:40:31 -0400

    Fixed add to cart redirection missing

--- controllers/shop.js
+++ controllers/shop.js
@@ -72,7 +72,7 @@ exports.postCart = (req, res, next) => {
         return req.user.addToCart(product);
       })
       .then( result => {
-        console.log('postCart', result)
+        res.redirect('/cart');
       })
       .catch( err => console.log(err));
     // let fetchedCart;
