65517f08e3b14c38420e4ab4cea16b0f594687be
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-19 06:37:34 -0400

    Added the request driven login

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,7 +7,8 @@ exports.getAddProduct = (req, res, next) => {
         pageTitle: "Add Product", 
         path: '/admin/add-product', 
         activeAddProduct: true,
-        editing: false
+        editing: false,
+        isAuthenticated: req.isLoggedIn
     });
 };
 
@@ -21,7 +22,7 @@ exports.postAddProduct = (req, res, next) => {
       price: price,
       description: description,
       imageUrl: imageUrl,
-      userId: req.user
+      userId: req.user,
     });
     product.save()
         .then( result => {
@@ -42,7 +43,8 @@ exports.getProducts = (req, res, next) => {
         res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
-            path: '/admin/products'
+            path: '/admin/products',
+            isAuthenticated: req.isLoggedIn
         })
     })
     .catch(err => {
@@ -67,7 +69,8 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: "Edit Product", 
                 path: '/admin/edit-product', 
                 editing: editMode,
-                product: product
+                product: product,
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch( err => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -16,7 +16,8 @@ exports.getIndex = (req, res, next) => {
             res.render('shop/index', { 
                 prods: products, 
                 pageTitle: 'Shop', 
-                path: '/'
+                path: '/',
+                isAuthenticated: req.isLoggedIn
             });
         }
       )
@@ -32,7 +33,8 @@ exports.getProducts = (req, res, next) => {
         res.render('shop/product-list', { 
             prods: products, 
             pageTitle: 'All Products', 
-            path: '/products', 
+            path: '/products',
+            isAuthenticated: req.isLoggedIn 
         });
     })
     .catch( err => {
@@ -47,7 +49,8 @@ exports.getProduct = (req, res, next) => {
         res.render('shop/product-detail', { 
             product: product, 
             pageTitle: 'Product Details', 
-            path: '/products'
+            path: '/products',
+            isAuthenticated: req.isLoggedIn
         });
     })
     .catch((err) => {
@@ -65,7 +68,8 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
           path: '/cart',
           pageTitle: 'Your Cart',
-          products: products
+          products: products,
+          isAuthenticated: req.isLoggedIn
       })
     })
     .catch( err => console.log(err));
@@ -127,7 +131,8 @@ exports.getOrders = (req, res, next) => {
       res.render('shop/orders', {
           path: '/orders',
           pageTitle: 'Orders',
-          orders: orders
+          orders: orders,
+          isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
