0a19f4b66d24d14ba227c10e0c558296d9e9a2b6
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-20 12:13:15 -0400

    Seting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,20 @@
+const { get } = require("mongoose");
+
 exports.getLogin = (req, res, next) => {
+
+  const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1];
+
+  console.log('isLoggedIn', isLoggedIn);
+
   res.render('auth/login', {  
       path: '/login',
       pageTitle: 'Login',
-      isAuthenticated: req.isLoggedIn
+      isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  // req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true')
   res.redirect('/');
 };
