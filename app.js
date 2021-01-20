7df65ed6f565396cdba208b5bddeee280b2c4e67
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-20 16:09:35 -0400

    Added mongoDB to store sessions

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 // const http = require('http');
 require('dotenv').config();
 const express = require('express');
-const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -15,18 +14,31 @@ const User = require('./models/user');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const MONGODB_URI = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@cluster0.xeicl.mongodb.net/shop';
+
+const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+app.use(session({ 
+    secret: 'my_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ 
-    secret: 'my_secret',
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+
 
 app.use((req, res, next) => {
     User.findById('5ffd6f5174215211ef56d625')
@@ -44,7 +56,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@cluster0.xeicl.mongodb.net/shop?retryWrites=true&w=majority')
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if(!user) {
