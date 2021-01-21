d95616e2ebf22443f00308ac3b240f79049123ee
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-21 10:01:04 -0400

    Deleting a session

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
-const authController = require('../controllers/auth')
+const authController = require('../controllers/auth');
+const { route } = require('./admin');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
+router.post('/logout', authController.postLogout);
 
 module.exports = router;
