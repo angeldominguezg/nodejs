091ac91ae4bc1682c93eb3a05f8cfc6668579097
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-23 11:56:51 -0400

    fixed auth and navigation styles

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,14 @@ const { route } = require('./admin');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
 router.post('/login', authController.postLogin);
+
+router.post('/signup', authController.postSignup);
+
 router.post('/logout', authController.postLogout);
+
 
 module.exports = router;
