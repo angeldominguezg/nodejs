17686daef9b49810f6ec81b88d58fb4278c64828
Angel Dominguez <angeldominguezg@gmail.com>
2021-01-28 05:30:44 -0400

    Erros messages for signing and signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,18 +3,31 @@ const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {  
+  let message = req.flash('error');
+  if( message.length > 0 ){
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render('auth/login', { 
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: req.flash('error')
+      errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if( message.length > 0 ){
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    isAuthenticated: false,
+    errorMessage: message
   });
 };
 
@@ -42,6 +55,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             })
           }
+          req.flash('error', 'Invalid email or password.');
           res.redirect('/login');
         })
         .catch( err => {
@@ -60,6 +74,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({email: email})
     .then(userDoc => {
       if (userDoc){
+        req.flash('error', 'E-mail exists already, please pick a different one.');
         return res.redirect('/signup');
       } else {
         return bcryptjs
