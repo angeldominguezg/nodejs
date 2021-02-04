fc553e8ca42693248d9d0a3d2fa057bd454bf5df
Angel Dominguez <angeldominguezg@gmail.com>
2021-02-04 16:41:36 -0400

    Fix update products

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,7 +85,7 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(productId)
     .then((product) => {
-      if(product.userId !== req.user._id){
+      if(product.userId.toString() !== req.user._id.toString()){
         return res.redirect('/');
       }
       product.title = updatedTitle;
