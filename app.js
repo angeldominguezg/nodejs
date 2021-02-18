ceb74dc8e08acd810d193df4cb6010f3f1607416
Angel Dominguez <angeldominguezg@gmail.com>
2021-02-18 11:07:16 -0400

    Fixed redirect to error 500

--- app.js
+++ app.js
@@ -44,14 +44,21 @@ app.use(session({
 app.use(csrfProtection);
 app.use(flash());
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 // User session middleware
 app.use((req, res, next)=>{
   if(!req.session.user){
     return next();
   }
+  // throw new Error('Dummy');
   User.findById(req.session.user._id)
     .then( user => {
+      // throw new Error('Dummy');
       if(!user){
         return next();
       }
@@ -59,15 +66,10 @@ app.use((req, res, next)=>{
       next();
     })
     .catch(err => {
-      throw new Error(err);
+      // throw new Error(err);
+      next(new Error(err));
     })
 });
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -77,7 +79,12 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect('/500');
+  res.status(500).render('500', 
+  { 
+    pageTitle: 'Error!', 
+    path: '/500', 
+    isAuthenticated: req.session.isLoggedIn 
+  });
 });
 
 mongoose
